a7152e013891e25d964d225eaf1309d86bdc1c44
omar <omar@sahab.ma>
2023-11-19 01:25:54 +0000

    core update

--- src/core/stress.py
+++ src/core/stress.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sympy as sp
 
-from tensor import SymmetricThreeByThreeTensor
+from .tensor import SymmetricThreeByThreeTensor
 
 
 class StressTensor(SymmetricThreeByThreeTensor):
@@ -9,6 +9,40 @@ class StressTensor(SymmetricThreeByThreeTensor):
         # Call the constructor of the base class
         super().__init__(data)
 
+    def normal_components(self):
+        return self.data[:3]
+
+    def shear_components(self):
+        return self.data[3:]
+
     def pressure(self):
-        # Calculate pressure from the trace of the stress tensor
-        return np.trace(self.data) / 3.0
+        return self.normal_components().sum() / 3.0
+
+    def tresca(self):
+        sigma_11, sigma_22, sigma_33, sigma_23, sigma_13, sigma_12 = self.data
+
+        max_shear_stress = max(
+            abs(sigma_11 - sigma_22),
+            abs(sigma_22 - sigma_33),
+            abs(sigma_33 - sigma_11),
+            abs(sigma_23),
+            abs(sigma_13),
+            abs(sigma_12),
+        )
+
+        return max_shear_stress
+
+    def von_mises(self):
+        sigma_11, sigma_22, sigma_33, sigma_23, sigma_13, sigma_12 = self.data
+
+        von_mises = np.sqrt(
+            0.5
+            * (
+                (sigma_11 - sigma_22) ** 2
+                + (sigma_22 - sigma_33) ** 2
+                + (sigma_33 - sigma_11) ** 2
+                + 6 * (sigma_23**2 + sigma_13**2 + sigma_12**2)
+            )
+        )
+
+        return von_mises
--- src/core/tensor.py
+++ src/core/tensor.py
@@ -3,21 +3,14 @@ import sympy as sp
 
 
 class ThreeByThreeTensor:
-    def __init__(self, data):
-        # Ensure that data is a 3x3 array
-        if not isinstance(data, np.ndarray) or data.shape != (3, 3):
+    def __init__(self, data: np.ndarray):
+        if isinstance(data, np.ndarray) and data.shape == (3, 3):
+            self.data = data
+        else:
             raise ValueError("Input data must be a 3x3 NumPy array")
-
-        self.data = data
 
     def __repr__(self):
         return f"ThreeByThreeTensor(\n{self.data}\n)"
-
-    def __add__(self, other):
-        pass
-
-    def __mul__(self, scalar):
-        pass
 
     @classmethod
     def create(cls, components):
@@ -37,34 +30,61 @@ class ThreeByThreeTensor:
         else:
             raise ValueError("Input must be a list")
 
+    def is_symmetric(self):
+        return np.array_equal(self.data, self.data.T)
 
-class SymmetricThreeByThreeTensor(ThreeByThreeTensor):
-    def __init__(self, data):
-        # Ensure that the tensor is symmetric
-        if not np.allclose(data, data.T):
-            raise ValueError("Input data must be a symmetric 3x3 tensor")
+    def to_symmetric(self):
+        if self.is_symmetric():
+            data = np.array(
+                [
+                    self.data[0, 0],
+                    self.data[1, 1],
+                    self.data[2, 2],
+                    self.data[2, 1],
+                    self.data[2, 0],
+                    self.data[1, 0],
+                ]
+            )
+            data = data.reshape((6, 1))  # Ensure the shape is (6, 1)
+            return SymmetricThreeByThreeTensor(data)
+        raise ValueError(
+            "The tensor is not symmetric, and it cannot be converted to a symmetric tensor."
+        )
 
-        # Call the constructor of the base class
-        super().__init__(data)
+
+class SymmetricThreeByThreeTensor:
+    def __init__(self, data: np.ndarray):
+        if isinstance(data, np.ndarray) and data.shape in [(6,), (6, 1)]:
+            self.data = data
+        else:
+            raise ValueError("Input data must be a 6x1 tensor")
 
     def __repr__(self):
         return f"SymmetricThreeByThreeTensor(\n{self.data}\n)"
-    
+
     @classmethod
-    def create(cls, components):
+    def create(cls, components: list):
         if isinstance(components, list) and len(components) == 6:
-            # Create a 3x3 symmetric tensor from a flat list of 6 components
-            data = np.array([[components[0], components[1], components[2]],
-                              [components[1], components[3], components[4]],
-                              [components[2], components[4], components[5]]])
+            data = np.array(components)
             return cls(data)
         else:
-            raise ValueError("Invalid input for creating a symmetric 3x3 tensor. Provide a list of 6 components.")
+            raise ValueError(
+                "Invalid input for creating a symmetric 3x3 tensor. Provide a list of 6 components."
+            )
+
+    def to_general_tensor(self):
+        components = [
+            [self.data[0], self.data[5], self.data[4]],
+            [self.data[5], self.data[1], self.data[3]],
+            [self.data[4], self.data[3], self.data[2]],
+        ]
+        data = np.array(components)
+        data = data.reshape((3, 3))
+        return ThreeByThreeTensor(data)
 
 
 class SymbolicThreeByThreeTensor:
     def __init__(self, data: sp.Matrix):
-        # Ensure that data is a 3x3 matrix
         if not isinstance(data, sp.Matrix) or data.shape != (3, 3):
             raise ValueError("Input data must be a 3x3 SymPy Matrix")
 
@@ -73,23 +93,15 @@ class SymbolicThreeByThreeTensor:
     def __repr__(self):
         return f"SymbolicThreeByThreeTensor(\n{self.data}\n)"
 
-    def __add__(self, other):
-        pass
-
-    def __mul__(self, scalar):
-        pass
-
     @classmethod
-    def create(cls, components):
+    def create(cls, components: list):
         if isinstance(components, list):
             if len(components) == 9:
-                # Create a 3x3 tensor from a flat list of 9 components
                 data = sp.Matrix(components).reshape(3, 3)
                 return cls(data)
             elif len(components) == 3 and all(
                 isinstance(row, list) and len(row) == 3 for row in components
             ):
-                # Create a 3x3 tensor from a list of 3 lists
                 data = sp.Matrix(components)
                 return cls(data)
             else:
@@ -97,69 +109,52 @@ class SymbolicThreeByThreeTensor:
         else:
             raise ValueError("Input must be a list")
 
+    def is_symmetric(self):
+        return self.data == self.data.transpose()
 
-class SymmetricSymbolicThreeByThreeTensor(SymbolicThreeByThreeTensor):
+    def to_symmetric(self):
+        if self.is_symmetric():
+            data = sp.Matrix(
+                [
+                    self.data[0, 0],
+                    self.data[1, 1],
+                    self.data[2, 2],
+                    self.data[2, 1],
+                    self.data[2, 0],
+                    self.data[1, 0],
+                ]
+            )
+            return SymmetricSymbolicThreeByThreeTensor(data)
+        raise ValueError(
+            "The tensor is not symmetric, and it cannot be converted to a symmetric tensor."
+        )
+
+
+class SymmetricSymbolicThreeByThreeTensor:
     def __init__(self, data: sp.Matrix):
-        # Ensure that the tensor is symmetric
-        if not data.is_symmetric():
-            raise ValueError("Input data must be a symmetric 3x3 tensor")
+        if not isinstance(data, sp.Matrix) or data.shape != (6, 1):
+            raise ValueError("Input data must be a 6x1 SymPy Matrix")
 
-        # Call the constructor of the base class
-        super().__init__(data)
+        self.data = data
+
+    def __repr__(self):
+        return f"SymmetricSymbolicThreeByThreeTensor(\n{self.data}\n)"
 
     @classmethod
-    def create(cls, components):
+    def create(cls, components: list):
         if isinstance(components, list) and len(components) == 6:
-            # Create a 3x3 symmetric tensor from a flat list of 6 symbolic components
-            data = sp.Matrix([[components[0], components[1], components[2]],
-                              [components[1], components[3], components[4]],
-                              [components[2], components[4], components[5]]])
+            data = sp.Matrix(components)
             return cls(data)
         else:
-            raise ValueError("Invalid input for creating a symbolic symmetric 3x3 tensor. Provide a list of 6 symbolic components.")
+            raise ValueError(
+                "Invalid input for creating a symbolic symmetric 3x3 tensor. Provide a list of 6 symbolic components."
+            )
 
-
-if __name__ == "__main__":
-    tensor1 = ThreeByThreeTensor.create([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    tensor2 = ThreeByThreeTensor.create([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
-    print("Tensor 1:")
-    print(tensor1)
-    print("\n")
-    print("Tensor 2:")
-    print(tensor2)
-    print("\n")
-
-
-    symmetric_tensor = SymmetricThreeByThreeTensor.create([1, 2, 3, 4, 5, 6])
-
-    print("Symmetric Tensor:")
-    print(symmetric_tensor)
-    print("\n")
-
-
-
-    a, b, c, d, e, f, g, h, i = sp.symbols("a b c d e f g h i")
-
-    symbolic_tensor1 = SymbolicThreeByThreeTensor.create([a, b, c, d, e, f, g, h, i])
-
-    symbolic_tensor2 = SymbolicThreeByThreeTensor.create(
-        [
-            [a, b, c],
-            [d, e, f],
-            [g, h, i],
+    def to_general_tensor(self):
+        components = [
+            [self.data[0], self.data[5], self.data[4]],
+            [self.data[5], self.data[1], self.data[3]],
+            [self.data[4], self.data[3], self.data[2]],
         ]
-    )
-
-    print("Symbolic Tensor 1:")
-    print(symbolic_tensor1)
-    print("\n")
-    print("Symbolic Tensor 2:")
-    print(symbolic_tensor2)
-    print("\n")
-
-    symbolic_symmetric_tensor = SymmetricSymbolicThreeByThreeTensor.create([a, b, c, d, e, f])
-
-    print("Symbolic Symmetric Tensor:")
-    print(symbolic_symmetric_tensor)
-    print("\n")
+        data = sp.Matrix(components)
+        return SymbolicThreeByThreeTensor(data)
