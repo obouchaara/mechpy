229935d8ccc85aaefb8431e58110a4f4325e7b4b
omar <omar@sahab.ma>
2023-11-22 02:15:44 +0000

    add hooke's law

--- src/core/elasticity.py
+++ src/core/elasticity.py
@@ -1,9 +1,20 @@
 import numpy as np
 import sympy as sp
 
-from .strain import StrainTensor, SymbolicStrainTensor
-from .stress import StressTensor, SymbolicStressTensor
-from .material import IsotropicMaterial
+from .strain import StrainTensor
+from .stress import StressTensor
+from .material import ComplianceTensor, StiffnessTensor
+
 
 class LinearElasticity:
-    pass
+    @staticmethod
+    def hookes_law(
+        compliance_tensor: ComplianceTensor, strain_tensor: StrainTensor
+    ) -> StressTensor:
+        return StressTensor(np.matmul(compliance_tensor.data, strain_tensor.data))
+
+    @staticmethod
+    def hookes_law_inverse(
+        stiffness_tensor: StiffnessTensor, stress_tensor: StressTensor
+    ) -> StrainTensor:
+        return StrainTensor(np.matmul(stiffness_tensor.data, stress_tensor.data))
