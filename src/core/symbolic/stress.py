b23538965de9b5ae09787f08d849b94e99cb4201
omar <omar@sahab.ma>
2023-11-27 12:30:10 +0000

    add symbolic yield criterion

--- src/core/symbolic/stress.py
+++ src/core/symbolic/stress.py
@@ -19,3 +19,24 @@ class SymbolicStressTensor(SymbolicSymmetricThreeByThreeTensor):
 
     def pressure(self):
         return sum(self.normal_components()) / 3.0
+    
+    def tresca(self):
+        principal_stresses = self.to_general_tensor().data.eigenvals()
+
+        principal_stresses = sp.Matrix(list(principal_stresses.keys()))
+
+        max_diff = sp.Max(*[sp.Abs(stress1 - stress2) for stress1 in principal_stresses for stress2 in principal_stresses])
+
+        return max_diff
+
+    def von_mises(self):
+        sigma_11, sigma_22, sigma_33, sigma_23, sigma_13, sigma_12 = self.data
+
+        # Von Mises Stress calculation for a symmetric stress tensor
+        von_mises_stress = sp.sqrt(
+            ((sigma_11 - sigma_22)**2 + (sigma_22 - sigma_33)**2 + (sigma_33 - sigma_11)**2) / 2
+            + 3 * (sigma_23**2 + sigma_13**2 + sigma_12**2)
+        )
+
+        return von_mises_stress
+
