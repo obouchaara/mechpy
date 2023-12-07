ea7a6bc4f38a798fad019e3b29c609fbc932b8cb
omar <omar@sahab.ma>
2023-12-07 13:40:35 +0000

    update docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["pydata_sphinx_theme"]
+extensions = []
 
 templates_path = ["_templates"]
 exclude_patterns = []
