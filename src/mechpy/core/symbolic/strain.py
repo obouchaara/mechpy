dded8d161b20587cbc98f038c602012766305c7e
obouchaara <o.bouchaara@sahab-dev.com>
2023-12-19 16:53:34 +0000

    add mode to symbolic strain tensor

--- src/mechpy/core/symbolic/strain.py
+++ src/mechpy/core/symbolic/strain.py
@@ -1,24 +1,33 @@
 import sympy as sp
 
-from .tensor import SymbolicSymmetricThreeByThreeTensor
+from .tensor import SymbolicSymmetricThreeByThreeTensor as SS3X3T
 
 
-class SymbolicStrainTensor(SymbolicSymmetricThreeByThreeTensor):
-    STRAIN_VOIGT_MAPPING = {
-        "\\epsilon_1": sp.symbols("\\epsilon_11"),
-        "\\epsilon_2": sp.symbols("\\epsilon_22"),
-        "\\epsilon_3": sp.symbols("\\epsilon_33"),
-        "\\epsilon_4": sp.symbols("\\epsilon_23") * 2,
-        "\\epsilon_5": sp.symbols("\\epsilon_13") * 2,
-        "\\epsilon_6": sp.symbols("\\epsilon_12") * 2,
-    }
+class SymbolicStrainTensor(SS3X3T):
+    STRAIN_VOIGT_MAPPING = {0: 1, 1: 1, 2: 1, 3: 2, 4: 2, 5: 2}
 
-    def __init__(self, data):
-        super().__init__(data)
+    def __init__(self, data, mode=0):
+        if mode in [0, 1]:
+            super().__init__(data)
+            self.mode = mode
+        else:
+            raise NotImplementedError(f"Mode {mode} not implemented")
 
     @classmethod
-    def create(cls, name="\\epsilon", mode=0):
-        return super().create(name, mode)
+    def create(cls, name="\epsilon", mode=0):
+        if mode == 0:
+            strain_tensor = super().create(name, mode=0)
+        elif mode == 1:
+            strain_tensor = super().create(name, mode=1)
+            data = strain_tensor.data
+            mapping = cls.STRAIN_VOIGT_MAPPING
+            components = [data[key] * value for key, value in mapping.items()]
+            new_data = sp.ImmutableDenseNDimArray(components)
+            strain_tensor = cls(new_data, 1)
+        else:
+            raise NotImplementedError(f"Mode {mode} not implemented")
+
+        return strain_tensor
 
     def normal_components(self):
         return self.data[:3]
@@ -30,6 +39,10 @@ class SymbolicStrainTensor(SymbolicSymmetricThreeByThreeTensor):
         return sum(self.normal_components())
 
     def to_general(self):
-        general_tensor = super().to_general()
-        general_tensor.data = general_tensor.data.subs(self.STRAIN_VOIGT_MAPPING)
-        return general_tensor
+        if self.mode == 1:
+            data = self.data
+            mapping = self.STRAIN_VOIGT_MAPPING
+            components = [data[k] / v for k, v in mapping.items()]
+            return SS3X3T.from_list(components).to_general()
+        else:
+            raise NotImplementedError(f"Only implemented for mode 1")
