d0422f321edaeb1973abb52913819ef3839c1bf4
obouchaara <o.bouchaara@sahab-dev.com>
2024-01-07 02:14:05 +0000

    correct the hook's laws and update notebook

--- src/mechpy/core/symbolic/elasticity.py
+++ src/mechpy/core/symbolic/elasticity.py
@@ -8,18 +8,6 @@ from .material import SymbolicComplianceTensor, SymbolicStiffnessTensor
 class SymbolicLinearElasticity:
     @staticmethod
     def hookes_law(
-        stiffness_tensor: SymbolicStiffnessTensor,
-        strain_tensor: SymbolicStrainTensor,
-    ) -> SymbolicStressTensor:
-        stiffness_matrix = sp.Matrix(stiffness_tensor.data.tolist())
-        strain_matrix = sp.Matrix(strain_tensor.data.tolist())
-        stress_matrix = stiffness_matrix @ strain_matrix
-        stress_data = sp.ImmutableDenseNDimArray(stress_matrix, (6,))
-        stress_tensor = SymbolicStressTensor(stress_data, notation=2)
-        return stress_tensor
-
-    @staticmethod
-    def hookes_law_inverse(
         compliance_tensor: SymbolicComplianceTensor,
         stress_tensor: SymbolicStressTensor,
     ) -> SymbolicStrainTensor:
@@ -29,4 +17,15 @@ class SymbolicLinearElasticity:
         strain_data = sp.ImmutableDenseNDimArray(strain_matrix, (6,))
         strain_tensor = SymbolicStressTensor(strain_data, notation=2)
         return strain_tensor
-    
+
+    @staticmethod
+    def hookes_law_inverse(
+        stiffness_tensor: SymbolicStiffnessTensor,
+        strain_tensor: SymbolicStrainTensor,
+    ) -> SymbolicStressTensor:
+        stiffness_matrix = sp.Matrix(stiffness_tensor.data.tolist())
+        strain_matrix = sp.Matrix(strain_tensor.data.tolist())
+        stress_matrix = stiffness_matrix @ strain_matrix
+        stress_data = sp.ImmutableDenseNDimArray(stress_matrix, (6,))
+        stress_tensor = SymbolicStressTensor(stress_data, notation=2)
+        return stress_tensor
